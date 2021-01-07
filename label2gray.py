313073528a7c2f3550dfa9f08b66ea2039454625
Fenghaze <feng_haze@163.com>
2021-01-07 17:12:10 +0800

    labelme

---
+++ label2gray.py
@@ -0,0 +1,84 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : label2gray.py
+@Time    : 2020/12/16 9:36
+@Author  : Zhl
+@Desc    : 
+"""
+
+import os
+import PIL.Image
+import numpy as np
+from skimage import io, data, color
+from skimage.io import imread, imsave
+from PIL import Image
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os.path as osp
+
+# 为生成的label上色
+def draw_color(label_path):
+    img_lst = os.listdir(label_path)
+    for img in img_lst:
+        save_path = os.path.join(label_path, img)
+        img_path = os.path.join(label_path, img)
+        img = PIL.Image.open(img_path)
+        img = np.array(img)
+        dst = color.label2rgb(img, bg_label=0, bg_color=(0, 0, 0))  # 背景的标签为0，颜色为黑色。分割多个类别时，查看skimage原函数
+        io.imsave(save_path, dst)
+        #edit_pixel_color(img_path, save_path)
+
+# 修改目标的像素颜色
+def edit_pixel_color(img, save_path):
+    i = 1
+    j = 1
+    img = Image.open(img)  # 读取系统的内照片
+    width = img.size[0]  # 长度
+    height = img.size[1]  # 宽度
+    for i in range(0, width):  # 遍历所有长度的点
+        for j in range(0, height):  # 遍历所有宽度的点
+            data = (img.getpixel((i, j)))  # 打印该图片的所有点
+            if (data[0]==1):  # 判断RGBA的R值
+                # 判断条件就是一个像素范围范围
+                img.putpixel((i,j),(255,0,0))#则这些像素点的颜色改成白色
+    img = img.convert("RGB")  # 把图片强制转成RGB
+    img.save(save_path)  # 保存修改像素点后的图片
+
+# 将RGB转为灰度图
+def convert_from_color_segmentation(img):
+  arr_2d = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+  palette = {
+    (  0,   0,   0) : 0 ,   # class：背景；  color：0,0,0；   label：0
+    (255,   0,   0) : 1     # class：ship;  color：255,0,0； label：1
+    }
+  for c, i in palette.items():
+    m = np.all(img == np.array(c).reshape(1, 1, 3), axis=2)
+    arr_2d[m] = i
+  return arr_2d
+
+# 生成灰度图
+def rgb2gray(label_path, gray_path):
+    img_lst = os.listdir(label_path)
+    for img in img_lst:
+        gray_label_path = os.path.join(gray_path, img)
+        img = imread(os.path.join(label_path, img))
+        if len(img.shape) > 2:
+            img_gray = convert_from_color_segmentation(img)
+            imsave(os.path.join(gray_label_path), img_gray)
+
+# 测试生成的灰度图是否成功
+def test_gray(label_img):
+    img = Image.open(label_img)
+    img = np.array(img)
+    print(np.unique(img))
+
+def mkdir(dir):
+    if not osp.exists(dir):
+        os.mkdir(dir)
+
+if __name__ == '__main__':
+    label_path = r'D:\Deeplearning\data\waterline\waterline_voc\SegmentationClass'
+    gray_path = r'D:\Deeplearning\data\waterline\waterline_val\json\out\gray'
+    #mkdir(gray_path)
+    #draw_color(label_path)
+    #rgb2gray(label_path, gray_path)
---
+++ run.py
@@ -0,0 +1,85 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : run.py
+@Time    : 2020/12/16 9:07
+@Author  : Zhl
+@Desc    : 
+"""
+
+import os
+import argparse
+import base64
+import json
+import os
+import os.path as osp
+
+import imgviz
+import PIL.Image
+
+from labelme.logger import logger
+from labelme import utils
+
+def mkdir(dir):
+    if not osp.exists(dir):
+        os.mkdir(dir)
+
+def json2img(json_file, out, index):
+    if out is None:
+        out_dir = osp.basename(json_file).replace(".", "_")
+        out_dir = osp.join(osp.dirname(json_file), out_dir)
+    else:
+        out_dir = out
+    mkdir(out_dir)
+    data = json.load(open(json_file))
+    imageData = data.get("imageData")
+
+    if not imageData:
+        imagePath = os.path.join(os.path.dirname(json_file), data["imagePath"])
+        with open(imagePath, "rb") as f:
+            imageData = f.read()
+            imageData = base64.b64encode(imageData).decode("utf-8")
+    img = utils.img_b64_to_arr(imageData)
+
+    label_name_to_value = {"_background_": 0}
+    for shape in sorted(data["shapes"], key=lambda x: x["label"]):
+        label_name = shape["label"]
+        if label_name in label_name_to_value:
+            label_value = label_name_to_value[label_name]
+        else:
+            label_value = len(label_name_to_value)
+            label_name_to_value[label_name] = label_value
+    lbl, _ = utils.shapes_to_label(
+        img.shape, data["shapes"], label_name_to_value
+    )
+
+    label_names = [None] * (max(label_name_to_value.values()) + 1)
+    for name, value in label_name_to_value.items():
+        label_names[value] = name
+
+    lbl_viz = imgviz.label2rgb(
+        label=lbl, img=imgviz.asgray(img), label_names=label_names, loc="rb"
+    )
+    img_name = str(index) + '.jpg'
+    out_dir_raw  = out_dir + '/raw'
+    out_dir_label = out_dir + '/label'
+    mkdir(out_dir_raw)
+    mkdir(out_dir_label)
+    PIL.Image.fromarray(img).save(osp.join(out_dir_raw, img_name))  #保存图片 1.jpg
+    utils.lblsave(osp.join(out_dir_label, img_name.replace('.jpg', '.png')), lbl) #保存标签 1.png
+    #PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, "label_viz.png"))
+
+    with open(osp.join(out_dir, "label_names.txt"), "w") as f:
+        for lbl_name in label_names:
+            f.write(lbl_name + "\n")
+
+    logger.info("Saved to: {}".format(out_dir))
+
+if __name__ == '__main__':
+
+    json_path = r'D:\Deeplearning\data\waterline\waterline_val\json'
+    out = r'D:\Deeplearning\data\waterline\waterline_val\json\out'
+    json_files = os.listdir(json_path)
+
+    for (index, file) in enumerate(json_files):
+        json_file = os.path.join(json_path, file)
+        json2img(json_file, out, index+1)
